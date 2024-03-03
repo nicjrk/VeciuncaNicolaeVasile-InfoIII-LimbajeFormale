e4d5ebbf4cff3bba6449f65d171ee5b69a61a241
nicjrk <116735056+nicjrk@users.noreply.github.com>
2024-03-03 21:25:14 +0200

    Add files via upload

---
+++ lab3x1/lab3x1/lab3.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    string input; 
+    int state = 0; 
+
+    cout << "Bine ati venit la automatul de bauturi. Alegeti o bautura:\n";
+    cout << "C - Cafea, T - Ceai, A - Cappuccino, H - Ciocolata calda, OK - Confirmare\n";
+
+    while (true) {
+        cout << "Selectie: ";
+        cin >> input; 
+
+        if (input == "X") { 
+            cout << "Automat oprit. La revedere!\n";
+            break;
+        }
+
+        switch (state) {
+        case 0: 
+            if (input == "C") {
+                state = 1; 
+                cout << "Cafea selectata. Apasati 'OK' pentru confirmare.\n";
+            }
+            else if (input == "T") {
+                state = 2; 
+                cout << "Ceai selectat. Apasati 'OK' pentru confirmare.\n";
+            }
+            else if (input == "A") {
+                state = 3;
+                cout << "Cappuccino selectat. Apasati 'OK' pentru confirmare.\n";
+            }
+            else if (input == "H") {
+                state = 4;
+                cout << "Ciocolata calda selectata. Apasati 'OK' pentru confirmare.\n";
+            }
+            break;
+        case 1: 
+            if (input == "OK") {
+                cout << "Cafeaua dumneavoastra este pregatita. Va multumim!\n";
+                state = 0; 
+            }
+            break;
+        case 2: 
+            if (input == "OK") {
+                cout << "Ceaiul dumneavoastra este pregatit. Va multumim!\n";
+                state = 0; 
+            }
+            break;
+        case 3: 
+            if (input == "OK") {
+                cout << "Cappuccino-ul dumneavoastra este pregatit. Va multumim!\n";
+                state = 0; 
+            }
+            break;
+        case 4:
+            if (input == "OK") {
+                cout << "Ciocolata calda dumneavoastra este pregatita. Va multumim!\n";
+                state = 0; 
+            }
+            break;
+        default:
+            cout << "Selectie invalida. Va rugam alegeti din nou.\n";
+            state = 0;
+        }
+    }
+
+    return 0;
+}
