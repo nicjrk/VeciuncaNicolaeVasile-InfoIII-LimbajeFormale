0961d3d63bff4e94fa5ebdc5fafccb7e19e864f2
nicjrk <116735056+nicjrk@users.noreply.github.com>
2024-03-18 10:21:27 +0200

    Add files via upload

---
+++ Lab5 ex1/Lab5 ex1/Lab5Ex1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+enum State { S1, S2 };
+enum Output { O1, O2 };
+
+class MooreMachine {
+    State currentState;
+
+public:
+    MooreMachine() : currentState(S1) {}
+
+    Output transition(char input) {
+        switch (currentState) {
+        case S1:
+            if (input == 'A') {
+                return O1;
+            }
+            else if (input == 'B') {
+                currentState = S2;
+                return O1;
+            }
+            break;
+        case S2:
+            if (input == 'A') {
+                currentState = S1;
+                return O2;
+            }
+            else if (input == 'B') {
+                return O2;
+            }
+            break;
+        }
+        return O1; 
+    }
+};
+
+int main() {
+    MooreMachine machine;
+    string inputs = "ABAB"; 
+
+    for (char input : inputs) {
+        Output output = machine.transition(input);
+        cout << "Input: " << input << ", Output: O" << (output == O1 ? "1" : "2") << endl;
+    }
+
+    return 0;
+}
