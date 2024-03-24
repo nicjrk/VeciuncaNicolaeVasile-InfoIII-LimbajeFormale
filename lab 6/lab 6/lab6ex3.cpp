fe82daddaa4c30cb323ef1f84a175ca0ceb6de1d
nicjrk <116735056+nicjrk@users.noreply.github.com>
2024-03-24 16:42:26 +0200

    Add files via upload

---
+++ lab 6/lab 6/lab6ex3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <regex>
+
+using namespace std;
+int main() {
+  
+    regex codPostalRegex("^[1-9][0-9][0-9]{3}$");
+
+
+    string codPostal = "12345";
+
+   
+    bool esteValid = regex_match(codPostal, codPostalRegex);
+
+    if (esteValid) {
+        cout << "Codul postal este valid.\n";
+    }
+    else {
+        cout << "Codul postal nu este valid.\n";
+    }
+
+    return 0;
+}
